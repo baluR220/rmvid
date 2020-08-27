c2deb9df6a6208b99d394cd0fa0b186dec0b1a2c
baluR220 <1qasw23edfr45gt6@gmail.com>
2020-08-27 17:11:43 +0700

    changed main to correctly handle input

--- remote_display/control_app.py
+++ remote_display/control_app.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import os
+import traceback
 
 
 try:
@@ -12,27 +13,69 @@ except ImportError:
     sys.exit()
 
 
-def main():
-    socket_file = os.path.join(work_dir, 'flow_text/text.socket')
+usage = '''
+Usage: <widget> <element> <set> <value>
+       <widget> <element> <get>
+       <widget> <command>
+       help
+       exit
+'''
+widgets = ['flow_text', 'video_player', 'slide_show', 'html_browser']
+
+
+def connection(socket_file):
     try:
         client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         client.settimeout(5)
         client.connect(socket_file)
     except Exception:
-        print('widget is not running')
-        sys.exit()
+        return 0
+    else:
+        return client
+
+
+def send_command(conn, command):
+    try:
+        command = ' '.join(command)
+        conn.send(command.encode('utf-8'))
+    except Exception:
+        return 0
+    else:
+        data = conn.recv(1024).decode('utf-8')
+        conn.close()
+        return data
+
+
+def main():
     while True:
-        data = input('> ')
-        try:
-            client.send(data.encode('utf-8'))
-        except Exception:
-            print('widget is not reachable', end='')
-        if data == 'exit':
-            client.close()
-            break
+        data = input('> ').split()
+        if data:
+            if data[0] == 'exit':
+                break
+            elif data[0] == 'help':
+                print(usage)
+            else:
+                if data[0] in widgets:
+                    widget = data[0]
+                    if len(data) > 1:
+                        socket_file = os.path.join(
+                            work_dir, '%s/text.socket' % widget
+                        )
+                        conn = connection(socket_file)
+                        if conn:
+                            response = send_command(conn, data[1:])
+                            if response:
+                                print(response)
+                            else:
+                                print('%s is not reachable' % widget)
+                        else:
+                            print('%s is not running.' % widget)
+                    else:
+                        print('not enough arguments. try "help"')
+                else:
+                    print('wrong widget name: %s' % data[0])
         else:
-            data = client.recv(1024).decode('utf-8')
-            print(data)
+            continue
 
 
 if __name__ == "__main__":
